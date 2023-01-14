425124f38c1208e92dd49b4744e3685a30bcc584
Austin <starshipsuperheavy23@gmail.com>
2023-01-14 13:24:44 -0500

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  plugins: [require("flowbite/plugin")],
+  theme: {},
+};
